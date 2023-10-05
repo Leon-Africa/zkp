facce3438370255574113882665615ae5e88dc62
Leon-Africa <mr.leon.africa@gmail.com>
2023-10-05 10:48:42 +0200

    extract request

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use num_bigint::BigUint;
 use tonic::{transport::Server, Code, Request, Response, Status};
 
 pub mod zkp_auth {
@@ -7,13 +8,45 @@ pub mod zkp_auth {
 use zkp_auth::{auth_server::{Auth, AuthServer}, RegisterRequest, RegisterResponse, AuthenticationChallengeRequest, AuthenticationAnswerResponse, AuthenticationAnswerRequest, AuthenticationChallengeResponse};
 
 #[derive(Debug, Default)]
-
 struct  AuthImpl {}
 
+#[derive(Debug, Default)]
+pub struct UserInfo {
+    pub user_name: String,
+
+    //registration
+    pub y1: BigUint,
+    pub y2: BigUint,
+
+    //authorization
+    pub r1: BigUint,
+    pub r2: BigUint,
+
+    //verification
+    pub c: BigUint,
+    pub s: BigUint,
+    pub session_id: BigUint
+
+}
 #[tonic::async_trait]
 impl Auth for AuthImpl {
     async fn register(&self, request: Request<RegisterRequest>) -> Result<Response<RegisterResponse>, Status> {
-        todo!()
+
+        //extract request
+        let request = request.into_inner();
+
+        //get request information
+        let username = request.user;
+        let y1 = BigUint::from_bytes_be(&request.y1);
+        let y2 = BigUint::from_bytes_be(&request.y2);
+
+        //intialize structure
+        let mut user_info = UserInfo::default();
+        user_info.user_name = username;
+        user_info.y1 = y1;
+        user_info.y2 = y2;
+
+        Ok(Response::new(RegisterResponse {  })) //defined in protobuf
     }
     
     async fn create_authentication_challenge(&self, request: Request<AuthenticationChallengeRequest>) -> Result<Response<AuthenticationChallengeResponse>, Status> {
